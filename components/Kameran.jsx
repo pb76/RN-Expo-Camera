adf4458daacf456cf7e56a13a526bf11d6abd042
Patrik Bernhardsson <pb76@users.noreply.github.com>
2025-10-02 13:35:55 +0200

    Skapade en kamera-komponent och lade till den i App.jsx

---
+++ components/Kameran.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Kameran() {
+  return (
+    <View style={styles.container}>
+      <Text>Här kommer vi jobba lite med kameran</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
